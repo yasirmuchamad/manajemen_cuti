f617198cb9d280c4692b272458779bd97f5be441
yassir <muchamad445@gmail.com>
2025-07-15 14:12:19 +0700

    tambahkan app manajemen_cuti dan models cuti

---
+++ manajemen_cuti/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ManajemenCutiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'manajemen_cuti'
---
+++ manajemen_cuti/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here
+class Cuti(models.Model):
+    """Model definition for Cuti."""
+    CUTI_CHOICES = [
+        ('tahunan', 'Tahunan'),
+        ('sakit', 'Sakit'),
+        ('darurat', 'Darurat')
+    ]
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('disetujui', 'Disetujui'),
+        ('ditolak', 'Ditolak'),
+    ]
+
+    # TODO: Define fields here
+    karyawan    = models.ForeignKey(User, on_delete = models.CASCADE)
+    jenis_cuti  = models.CharField(
+                    max_length = 16,
+                    choices = CUTI_CHOICES,
+                    default = 'pending'
+                    )
+    tgl_mulai   = models.DateTimeField(default = timezone.now)
+    tgl_selesai = models.DateTimeField()
+    status      = models.CharField(
+                    max_length = 16,
+                    choices = STATUS_CHOICES,
+                    default = 'tahunan'
+                    )
+    alasan      = models.CharField(max_length = 32, Null = True, Blank=True)
+
+    class Meta:
+        """Meta definition for Cuti."""
+
+        verbose_name = 'Cuti'
+        verbose_name_plural = 'Cutis'
+
+    def __str__(self):
+        """Unicode representation of Cuti."""
+        return f"{self.karyawan}-{self.jenis_cuti}-{self.status}"
